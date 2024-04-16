73036086f70646b174d55b258cc579d36e60a89c
shahkrish18 <shahkrish181003@gmail.com>
2024-04-16 20:06:28 +0530

    Graphql integrated

--- src/app/[name].js
+++ src/app/[name].js
@@ -1,14 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import React,{useState} from 'react'
 import { useLocalSearchParams } from 'expo-router'
 import exercises from '../../assets/data/exercises.json'
 import tw from 'twrnc'
+import {Stack} from 'expo-router'
 
 const ExerciseDetails = () => {
 
   const params = useLocalSearchParams();
   const exercise = exercises.find((item) => item.name === params.name)
-
+  const [isExpanded, setIsExpanded] = useState(false)
   if(!exercise) {
     return (
       <View>
@@ -18,13 +19,30 @@ const ExerciseDetails = () => {
   }
 
   return (
-    <View style={tw`p-5`} >
-      <Text style={tw`text-lg font-semibold text-black mb-1`}>{exercise.name}</Text>
+    <ScrollView style={styles.container}>
+      <Stack.Screen options={{title: exercise.name}} />
+      <View style={tw`bg-white rounded-lg p-2 `}>
+      <Text style={tw`text-lg font-semibold text-black `}>{exercise.name}</Text>
         <Text style={tw`text-gray-600 text-slate-300 text-transform:capitalize`}>
           {exercise.muscle} | {exercise.equipment}
         </Text>
-    </View>
+      </View>
+      <View style={tw`bg-white rounded-lg p-2 mt-2 mb-2`}>
+      <Text style={tw`leading-5 text-base`} numberOfLines={isExpanded ? 0: 3} >{exercise.instructions}</Text>
+      <Text onPress={()=> setIsExpanded(!isExpanded)} style={tw`self-center font-bold p-1`}>
+        {isExpanded ? 'see less': 'see more'}
+      </Text>
+      </View>
+    </ScrollView>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333945',
+    paddingHorizontal: 20,
+    paddingTop: 10,
+  },
+});
 export default ExerciseDetails
