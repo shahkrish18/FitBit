fa5261d64ab0fb92cca12645dd82d3463e3b0497
shahkrish18 <shahkrish181003@gmail.com>
2024-04-16 12:24:48 +0530

    First exercise done

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import exercises from './assets/data/exercises.json';
+import tw from 'twrnc';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default function App() {
+
+  const exercise = exercises[0];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={tw`rounded`}>
+        <Text style={tw`text-lg font-semibold text-white mb-1`}>{exercise.name}</Text>
+        <Text style={tw`text-gray-600 text-slate-300`}>{exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}</Text>
+        <Text>{exercise.name}</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#333945',
+    paddingHorizontal: wp('4%'),
     justifyContent: 'center',
   },
 });
